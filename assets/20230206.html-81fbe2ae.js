492688396230137f6dd5c09ed8fe17fd9a679899
RonenWen <wjqronen@gmail.com>
2023-02-06 16:26:54 +0800

    更新了培训

---
+++ assets/20230206.html-81fbe2ae.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-2196d9d2","path":"/front-end/list/20230206.html","title":"2023年2月6日","lang":"zh-CN","frontmatter":{"author":"James Yuan"},"headers":[{"level":2,"title":"培训详情","slug":"培训详情","link":"#培训详情","children":[]}],"git":{},"readingTime":{"minutes":0.83,"words":249},"filePathRelative":"front-end/list/20230206.md"}');export{t as data};
